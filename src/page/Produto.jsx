7efce526a8dcf77a713fba9edb1680a5e0df3207
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-05-08 20:17:14 -0300

    route2

---
+++ src/page/Produto.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+function Produto() {
+  return (
+    <Container>
+      <p>Produto</p>
+    </Container>
+  );
+}
+
+export { Produto };
--- src/route/index.jsx
+++ src/route/index.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ListaProdutos } from "../componentes/ListaProdutos";
+import { Produto } from "../page/Produto";
 
 function RouteApp() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ListaProdutos />} />
+        <Route path="/produto" element={<Produto />} />
       </Routes>
     </BrowserRouter>
   );
